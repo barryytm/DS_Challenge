27b278dd585f2540cef7738091a3946370cc233b
barryytm <barry.t.m.yung@gmail.com>
2019-07-06 04:04:52 -0400

    remote unneeded route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 let formRouter = require('./routes/form');
 let schoolRouter = require('./routes/school');
 
@@ -24,17 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/form', formRouter);
 app.use('/school', schoolRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
