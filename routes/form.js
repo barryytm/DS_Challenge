4e2a1c5cb26481b8bdfd5fc9ddb86335bb9f2695
barryytm <barry.t.m.yung@gmail.com>
2019-07-06 04:07:28 -0400

    fix S3

--- routes/form.js
+++ routes/form.js
@@ -27,7 +27,7 @@ router.post('/createSchool', (req, res) => {
 
 function uploadToS3(file) {
   let s3bucket = new aws.S3({
-    accessKeyId: AWS_ACCESS_KEY_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     Bucket: process.env.S3_BUCKET
   });
